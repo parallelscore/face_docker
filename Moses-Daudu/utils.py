f2b0a15e0c550d429c352611fd799efb317dfc6b
Moses Daudu <mosesdaudu001@gmail.com>
2023-04-28 12:34:49 +0100

    removed face_recognition
    Signed-off-by: Moses Daudu <mosesdaudu001@gmail.com>

--- Moses-Daudu/utils.py
+++ Moses-Daudu/utils.py
@@ -43,30 +43,3 @@ def youtube_downloader(youtube_link, folder_path):
     if files[-1].endswith('.mp4') or files[-1].endswith('.avi') or files[-1].endswith('.mov'):
         cap  = cv2.VideoCapture(file_path)
         return cap
-        
-def remove_duplicates(image_folder):
-    # create a list of face encodings for all images in the folder
-    face_encodings = []
-    for filename in os.listdir(image_folder):
-        image_path = os.path.join(image_folder, filename)
-        image = face_recognition.load_image_file(image_path)
-        encoding = face_recognition.face_encodings(image)
-        if len(encoding) > 0:
-            face_encodings.append(encoding[0])
-
-    # create a list of duplicate face encodings
-    duplicate_encodings = []
-    for i, encoding in enumerate(face_encodings):
-        for j in range(i+1, len(face_encodings)):
-            distance = face_recognition.face_distance([encoding], face_encodings[j])
-            if distance < 0.6:  # threshold for similarity
-                duplicate_encodings.append(j)
-
-    # remove duplicate images
-    for i in sorted(set(duplicate_encodings), reverse=True):
-        filename = os.listdir(image_folder)[i]
-        image_path = os.path.join(image_folder, filename)
-        os.remove(image_path)
-
-    print("Successfully Deleted duplicated images!!!")
-    
